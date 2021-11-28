904aded4b7767e741c9e6add422e7ecb87df3031
mariagaa993 <mariagaa993@gmail.com>
2021-11-28 01:01:52 -0400

    add error page styles

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -4,10 +4,10 @@ import { faFrown } from '@fortawesome/free-solid-svg-icons';
 
 const Initial = () => {
     return (
-        <Container fluid>  
-            <FontAwesomeIcon icon={faFrown} />
-            <h1>404</h1> 
-            <p>Page not found</p>
+        <Container fluid className='container-error'>  
+            <FontAwesomeIcon className='error-icon' icon={faFrown} />
+            <h1 className='error-title'>404</h1> 
+            <p className='error-p'>Page not found</p>
             <small>The Page you are looking for doesn't exist or an other error occurred.
                 Go back or choose a new direction.</small>  
         </Container>
